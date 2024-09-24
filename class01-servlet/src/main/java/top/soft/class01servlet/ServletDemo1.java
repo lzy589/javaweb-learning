6b3f6d9bb8c8922b5f5463243c6240178620a593
lzy <2360183839@qq.com>
2024-09-25 01:03:21 +0800

    servlet

---
+++ class01-servlet/src/main/java/top/soft/class01servlet/ServletDemo1.java
@@ -0,0 +1,54 @@
+    package top.soft.class01servlet;
+
+    import jakarta.servlet.*;
+
+    import java.io.IOException;
+
+    public class ServletDemo1 implements Servlet {
+        /**
+         * Servlet
+         * @param servletConfig
+         * @throws ServletException
+         */
+        @Override
+        public void init(ServletConfig servletConfig) throws ServletException {
+
+        }
+
+        @Override
+        public ServletConfig getServletConfig() {
+            return null;
+        }
+
+        /**
+         * 执行 Service 方法
+         * @param servletRequest
+         * @param servletResponse
+         * @throws ServletException
+         * @throws IOException
+         */
+        @Override
+        public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
+            System.out.println("service 执行");
+            System.out.println(servletRequest);
+        }
+
+        /**
+         * 获取 servlet 信息
+         *
+         * @return
+         */
+        @Override
+        public String getServletInfo() {
+            return "";
+        }
+
+        /**
+         * 执行销毁方法
+         */
+        @Override
+        public void destroy() {
+            System.out.println("销毁方法");
+
+        }
+    }
